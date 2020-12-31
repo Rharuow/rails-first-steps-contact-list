47c7ad64e00ee4529e18c82e73c554b6c8d9c8ee
Harysson Soares <haryssonsoares@gmail.com>
2020-12-31 11:25:17 -0300

    feat: layout static pages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,9 +5,8 @@
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import "jquery"
+import * as ActiveStorage from "@rails/activestorage" 
+import "bootstrap"
 
 Rails.start()
 Turbolinks.start()
