98b83aeffaf2a47e799abde779d185270e8b0fdb
Harysson Soares <haryssonsoares@gmail.com>
2021-01-01 13:14:18 -0300

    feat: user models

--- db/migrate/20210101155421_create_users.rb
+++ db/migrate/20210101155421_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :email
-      t.string :nome
-      t.string :password_digest
+      t.string :email, null: false, default: ''
+      t.string :nome,null: false, default: ''
+      t.string :password_digest, null: false, default: ''
 
       t.timestamps
     end
